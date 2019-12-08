68531cf073b2a3d14680e9cd0e1e24869f5158c7
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 16:36:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
  index = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
-end
+
 
 def summon_captain_planet(array)
     planeteer_calls = []
@@ -14,9 +14,12 @@ def summon_captain_planet(array)
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+   array.any? do |word|
+    word.length > 4
 end
+end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
